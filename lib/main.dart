5c452785f4bb45f27d114df0fe8c6dda6fccaf9f
Omnia-Waleed-Mohamed <omniaw03@gmail.com>
2025-07-17 04:00:10 +0300

    screen utils

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:marketi/core/constant/string.dart';
 import 'package:marketi/core/routing/router.dart';
 
 void main() {
@@ -14,6 +17,19 @@ class Marketi extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ScreenUtilInit(
+      designSize: Size(375,812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Marketi',
+        theme: ThemeData(),
+
+        onGenerateRoute: appRouter.generatRoute,
+        initialRoute: Routes.splash,
+        
+      ),
+    );
   }
 }
